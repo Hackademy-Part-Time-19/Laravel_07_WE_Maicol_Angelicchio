7ed9fab4d1b9ceb8de6c79acfd6388f76d8e2aed
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-02-13 20:43:57 +0100

    inserita validazione

--- 11_02_24_we/app/Http/Controllers/ArticleController.php
+++ 11_02_24_we/app/Http/Controllers/ArticleController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Article;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\StoreArticleRequest;
 
 class ArticleController extends Controller
 {
-    public function store(Request $request){
-
+    public function store(StoreArticleRequest $request){
         Article::create($request->all());
-        return view('form');
+        return redirect()->back()->with(['Succes'=>'Articolo inserito']);
     }
     public function create(){
         return view('form');
--- 11_02_24_we/resources/views/form.blade.php
+++ 11_02_24_we/resources/views/form.blade.php
@@ -4,15 +4,15 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Titolo:</label>
-            <input type="text" class="form-control" id="title" placeholder="Inserisci il titolo" name="title" required>
+            <input type="text" class="form-control" id="title" placeholder="Inserisci il titolo" name="title">
         </div>
         <div class="mb-3">
             <label for="category" class="form-label">Categoria:</label>
-            <input type="text" class="form-control" id="category" placeholder="Inserisci la categoria" name="category" required>
+            <input type="text" class="form-control" id="category" placeholder="Inserisci la categoria" name="category">
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione:</label>
-            <input type="text" class="form-control" id="description" placeholder="Inserisci la descrizione" name="description" required>
+            <input type="text" class="form-control" id="description" placeholder="Inserisci la descrizione" name="description">
         </div>
         <div class="mb-3">
             <label for="body" class="form-label">Body:</label>
